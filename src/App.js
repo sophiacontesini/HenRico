0a11b248884ae9505cfd023ebe4a1f621bb995b8
Sophia Contesini <sophiamarques214@gmail.com>
2023-08-04 11:29:24 -0300

    feat: include footer component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
+import Container from './components/layout/Container';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Projects from './components/pages/Projects';
+
 function App() {
   return (
-    <div className='App'>
-      <p>Pro-Spend</p>
-    </div>
+    <Router>
+      <Navbar />
+
+      <Container customClass='min-height'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/company' element={<Company />} />
+          <Route path='/newproject' element={<NewProject />} />
+        </Routes>
+      </Container>
+
+      <Footer />
+    </Router>
   );
 }
 
