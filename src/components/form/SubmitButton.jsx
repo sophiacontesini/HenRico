3a510f953a3b8f5c6be66f5397dfe618a863a006
Sophia Contesini <sophiamarques214@gmail.com>
2023-08-08 16:42:50 -0300

    feat: create submit button component

---
+++ src/components/form/SubmitButton.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './SubmitButton.module.css';
+
+export default function SubmitButton({ text }) {
+  return (
+    <div>
+      <button className={styles.btn}>{text}</button>
+    </div>
+  );
+}
