76719fcca19c4189cedcdc80d434d4ae50c21bc8
Sophia Contesini <sophiamarques214@gmail.com>
2023-08-08 16:45:57 -0300

    feat: add message in projects component

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,5 +1,19 @@
+import { useLocation } from 'react-router-dom';
+
 import React from 'react';
+import Message from '../layout/Message';
 
 export default function Projects() {
-  return <h1>Projects</h1>;
+  const location = useLocation();
+  let message = '';
+  if (location.state) {
+    message = location.state.message;
+  }
+
+  return (
+    <div>
+      <h1>Meus Projetos</h1>
+      {message && <Message msg={message} type='success' />}
+    </div>
+  );
 }
