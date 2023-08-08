e022266e965b65404ec218d89fee7a7890f26af5
Sophia Contesini <sophiamarques214@gmail.com>
2023-08-08 16:44:53 -0300

    feat:  include fetch in newproject component

--- src/components/pages/NewProject.jsx
+++ src/components/pages/NewProject.jsx
@@ -1,5 +1,37 @@
+import { useNavigate } from 'react-router-dom';
 import React from 'react';
+import ProjectForm from '../project/ProjectForm';
+import styles from './NewProject.module.css';
 
 export default function NewProject() {
-  return <h1>Novo Projeto</h1>;
+  const navigate = useNavigate();
+
+  function createPost(project) {
+    //initialize cost and services
+    project.cost = 0;
+    project.services = [];
+
+    fetch('http://localhost:8000/projects', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(project)
+    })
+      .then(resp => resp.json())
+      .then(data => {
+        console.log(data);
+        //redirect
+        navigate('/projects', { message: 'Projeto criado com sucesso!' });
+      })
+      .catch(err => console.log(err));
+  }
+
+  return (
+    <div className={styles.newproject_container}>
+      <h1>Criar Projeto</h1>
+      <p>Crie seu projeto para depois adicionar os serviços</p>
+      <ProjectForm handleSubmit={createPost} btnText='Criar Projeto' />
+    </div>
+  );
 }
