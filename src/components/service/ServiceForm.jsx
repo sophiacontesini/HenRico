cbd0139065e81ea91726dc49ccefdb624c80de74
Sophia Contesini <sophiamarques214@gmail.com>
2023-08-16 12:12:41 -0300

    feat: include serviceForm component

---
+++ src/components/service/ServiceForm.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { useState } from 'react';
+
+import Input from '../form/Input';
+import SubmitButton from '../form/SubmitButton';
+
+import styles from '../project/ProjectForm.module.css';
+
+export default function ServiceForm({ handleSubmit, btnText, projectData }) {
+  const [service, setService] = useState({});
+
+  function submit(e) {
+    e.preventDefault();
+    projectData.services.push(service);
+    handleSubmit(projectData);
+  }
+
+  function handleChange(e) {
+    setService({ ...service, [e.target.name]: e.target.value });
+  }
+
+  return (
+    <form onSubmit={submit} className={styles.form}>
+      <Input
+        type='text'
+        name='name'
+        text='Nome do serviço'
+        placeholder='Insira o nome do serviço'
+        handleOnChange={handleChange}
+      />
+      <Input
+        type='number'
+        name='cost'
+        text='Custo do serviço'
+        placeholder='Insira o valor total'
+        handleOnChange={handleChange}
+      />
+      <Input
+        type='text'
+        name='description'
+        text='Descrição do serviço'
+        placeholder='Descreva o serviço'
+        handleOnChange={handleChange}
+      />
+      <SubmitButton text={btnText} />
+    </form>
+  );
+}
