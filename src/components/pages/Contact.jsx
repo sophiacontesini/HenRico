937eddc93bef54c5edea6262c3c748e595a54246
Sophia Contesini <sophiamarques214@gmail.com>
2023-08-16 13:39:10 -0300

    feat: include contact page style

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import styles from './Contact.module.css';
 
 export default function Contact() {
-  return <h1>Contato</h1>;
+  return (
+    <div className={styles.contact_container}>
+      <h1>Contato</h1>
+      <p>Sophia Marques Contesini</p>
+      <p>Email: sophiamarques214@gmail.com</p>
+      <p>Telefone: 199 8264-2207</p>
+    </div>
+  );
 }
